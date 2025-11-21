e244a37a5fdae697e343edcc1f790ea2e5517f91
fishhunter29 <abhishekhmining@gmail.com>
2025-11-21 13:44:45 +0200

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/",           // important for Vercel / Netlify
+  build: {
+    outDir: "dist"
+  },
+  server: {
+    port: 5173
+  }
+});
